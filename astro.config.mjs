817312c73a246ff36da5a5c4fdcbab4f58fb46ec
wcgordon1 <wcgordon1@gmail.com>
2024-10-30 17:19:53 -0700

    Metas working properly. Fix email send form next

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,6 @@ export default defineConfig({
     skipInline: false,
     drafts: true
   },
-  site: 'https://lexingtonthemes.com',
+  site: 'https://productized.studio',
   integrations: [tailwind(), sitemap(), mdx()]
 });
